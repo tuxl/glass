93c8c0493d15fbbf89a546b68a997cc5ace9f0f0
tuxl <554032632@qq.com>
2019-03-19 15:40:39 +0800

    图片压缩

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -226,6 +226,7 @@
 
 					// Set background.
 						$image.css('background-image', 'url(' + $image_img.attr('src') + ')');
+						$image.css('background-size', 'contain');
 
 					// Set background position.
 						if (x = $image_img.data('position'))
@@ -236,49 +237,6 @@
 
 			});
 
-		// Poptrox.
-			/*$main.poptrox({
-				baseZIndex: 20000,
-				caption: function($a) {
-
-					var s = '';
-
-					$a.nextAll().each(function() {
-						s += this.outerHTML;
-					});
-
-					return s;
-
-				},
-				fadeSpeed: 300,
-				onPopupClose: function() { $body.removeClass('modal-active'); },
-				onPopupOpen: function() { $body.addClass('modal-active'); },
-				overlayOpacity: 0,
-				popupCloserText: '',
-				popupHeight: 150,
-				popupLoaderText: '',
-				popupSpeed: 300,
-				popupWidth: 150,
-				selector: '.thumb > a.image',
-				usePopupCaption: true,
-				usePopupCloser: true,
-				usePopupDefaultStyling: false,
-				usePopupForceClose: true,
-				usePopupLoader: true,
-				usePopupNav: true,
-				windowMargin: 50,
-
-
-			});*/
-
-			// Hack: Set margins to 0 when 'xsmall' activates.
-				/*breakpoints.on('<=xsmall', function() {
-					$main[0]._poptrox.windowMargin = 0;
-				});
-
-				breakpoints.on('>xsmall', function() {
-					$main[0]._poptrox.windowMargin = 50;
-				});*/
 
 })(jQuery);
 
